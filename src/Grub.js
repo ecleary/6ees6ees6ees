af9413190e13942d5f7d224496001b50d82ea13f
Errol Cleary <errol.cleary@gmail.com>
2020-12-06 21:29:58 -0800

    Implement HoneyMakerBee

--- src/Grub.js
+++ src/Grub.js
@@ -3,8 +3,8 @@ class Grub {
     this.age = 0;
     this.color = 'pink';
     this.food = 'jelly';
-  }
+  };
   eat() {
     return 'Mmmmmmmmm jelly';
-  }
+  };
 }
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
-};
+class HoneyMakerBee extends Bee {
+  constructor(food, eat) {
+    super(food, eat);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  };
+  makeHoney() {
+    this.honeyPot++;
+  };
+  giveHoney() {
+    this.honeyPot--;
+  };
+}
